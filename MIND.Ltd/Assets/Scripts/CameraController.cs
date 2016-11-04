9667c9e18a7a65fad73e232f8c84f7350dc652ef
adaws97 <alexander_dawson@ymail.com>
2016-11-04 21:13:47 +0000

    Add files via upload

---
+++ MIND.Ltd/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private const float speed = 3.0f;
+
+    // Use this for initialization
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float interpolation = Time.deltaTime * speed;
+
+        Vector3 position = this.transform.position;
+        position.y = Mathf.Lerp(this.transform.position.y, player.transform.position.y, interpolation);
+        position.x = Mathf.Lerp(this.transform.position.x, player.transform.position.x, interpolation);
+
+        this.transform.position = position;
+    }
+}
