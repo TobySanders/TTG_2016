02487a29ab4c00aea51912cb8c1d8f82e3fc8620
adaws97 <alexander_dawson@ymail.com>
2016-11-05 02:49:46 +0000

    Add files via upload

---
+++ MIND.Ltd/Assets/Scripts/PlayerAmmo.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAmmo : MonoBehaviour {
+
+    public int pistolAmmo,
+               shotgunAmmo,
+               grenadeAmmo,
+               machinegunAmmo;
+
+    public int startPistolAmmo,
+                startShotgunAmmo,
+                startGrenadeAmmo,
+                startMachinegunAmmo;
+
+    public string currentWeaponTag;
+
+    // Use this for initialization
+    void Start () {
+        pistolAmmo = startPistolAmmo;
+        shotgunAmmo = startShotgunAmmo;
+        grenadeAmmo = startGrenadeAmmo;
+        machinegunAmmo = startMachinegunAmmo;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col) {
+        System.Random random = new System.Random();
+        switch (col.gameObject.tag) {
+            case "pistolAmmo":
+                pistolAmmo += random.Next(5, 20);
+                col.gameObject.SetActive(false);
+                break;
+            case "shotgunAmmo":
+                shotgunAmmo += random.Next(5, 15);
+                col.gameObject.SetActive(false);
+                break;
+            case "grenadeAmmo":
+                grenadeAmmo += random.Next(1, 5);
+                col.gameObject.SetActive(false);
+                break;
+            case "machinegunAmmo":
+                machinegunAmmo += random.Next(5, 15);
+                col.gameObject.SetActive(false);
+                break;
+            default:
+                return;
+        }
+    }
+}
---
+++ MIND.Ltd/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startHealth = 100;
+    public int currentHealth;
+
+    // Use this for initialization
+    void Start() {
+        currentHealth = startHealth;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void TakeDamage(int amount) {
+        currentHealth -= amount;
+    }
+
+    private void Die() {
+        this.gameObject.SetActive(false);
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.tag == "health") {
+            currentHealth += 10;
+            col.gameObject.SetActive(false);
+        }
+    }
+}
